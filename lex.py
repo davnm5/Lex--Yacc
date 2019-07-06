5107362dd8165dd37d7a2b12769b21c751428a26
davnm5 <josdanei@espol.edu.ec>
2019-07-05 23:40:58 -0500

    [ADD] Validación de sentencias IF y presentación de resultados

---
+++ lex.py
@@ -0,0 +1,270 @@
+import re
+import numpy as np
+
+
+linea=0
+tokens = (
+    'NUMPY',
+    'NAME','NUMBER',
+    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
+    'LPAREN','RPAREN','POTENCY','DIVIDE_INT','STR','LIST', 'ARRAY', 'RESHAPE', 'SUM' , 'MEAN','POINT','PRINT','IF','ELSE','MENORQUE',
+    'TAB','DOSPUNTOS','MAYORQUE','DIFERENTE','DIGUAL','LINE','OR','AND'
+    )
+
+# Tokens
+t_POINT = r'\.'
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_EQUALS  = r'='
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_POTENCY = r'\*{2}'
+t_DIVIDE_INT = r'\/{2}'
+t_MENORQUE = r'\<'
+t_MAYORQUE= r'\>'
+t_TAB= r'[ \t]'
+
+
+def t_DOSPUNTOS(t):
+    r':'
+    t.value = t.value
+    return t
+
+def t_NUMPY(t):
+    r'np'
+    t.value = t.value
+    return t
+
+def t_AND(t):
+    r'and'
+    t.value = t.value
+    return t
+
+def t_OR(t):
+    r'or'
+    t.value = t.value
+    return t
+
+def t_PRINT(t):
+    r'print'
+    t.value = t.value
+    return t
+
+def t_IF(t):
+    r'if'
+    t.value = t.value
+    return t
+
+def t_ELSE(t):
+    r'else:'
+    t.value = t.value
+    return t
+    
+def t_MEAN(t) :
+    r'mean'
+    t.value = t.value
+    return t
+
+def t_SUM(t) :
+    r'sum'
+    t.value = t.value
+    return t
+
+def t_RESHAPE(t) :
+    r'reshape'
+    t.value = t.value
+    return t
+
+def t_ARRAY (t):
+    r'array'
+    t.value = t.value
+    return t
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_STR(t):
+    r'(\'(\s*\w*\S*)+\')|(\"(\s*\w*\S*)+\")'
+    t.value =t.value
+    return t
+
+def t_LIST(t):
+    r'(\[((\d+|\"\S*\W*\S*\")(\,(\d+|\"\S*\W*\S*\"))*)*\])'
+    t.value=t.value
+    return t
+
+def t_DIFERENTE(t):
+    r'!='
+    t.value=t.value
+    return t
+
+def t_DIGUAL(t):
+    r'=='
+    t.value=t.value
+    return t
+    
+    
+
+def t_error(t):
+    print("Caracter no válido '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+import ply.lex as lex
+lex.lex()
+
+
+precedence = (
+    ('right' , 'EQUALS'),
+    ('right' , 'NAME'),
+    ('right','NUMPY','POINT'),
+    ('left','PLUS','MINUS'),
+    ('left','TIMES','DIVIDE'),
+    ('right','UMINUS'),
+    )
+
+names = { }
+
+
+
+
+def p_statement_expr(p):
+    '''statement : expression
+                 | expresasign
+                 | if
+                 | print
+                 | else '''
+
+
+    aux=str(p[1])
+    print(re.sub('"', "", re.sub("'","",aux)))
+
+
+def p_print(p):
+    '''print : TAB PRINT LPAREN expression RPAREN
+            | PRINT LPAREN expression RPAREN
+            | ELSE print'''
+
+
+def p_statement_assign(p):
+    '''expresasign : NAME EQUALS expression
+                   | NAME EQUALS exprenumpy'''
+    names[p[1]] = p[3]
+
+def p_expression_binop(p):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression
+                  | expression POTENCY expression
+                  | expression DIVIDE_INT expression'''
+    try:
+        if p[2] == '+': p[0] = p[1] + p[3]
+        elif p[2] == '-': p[0] = p[1] - p[3]
+        elif p[2] == '*': p[0] = p[1] * p[3]
+        elif p[2] == '/': p[0] = p[1] / p[3]
+        elif p[2]== '**':  p[0]= p[1] ** p[3]
+        elif p[2]== '//':  p[0]= p[1] // p[3]
+
+        print(p[0])
+    except:
+        print("La operacion no es válida")
+
+
+def p_condition(p):
+    ''' condition    : expression DIGUAL expression
+    | expression DIFERENTE expression
+    | expression MAYORQUE expression
+    | expression MENORQUE expression
+    | condition TAB AND TAB condition
+    | condition TAB OR TAB condition '''
+
+def p_if(p):
+    ''' if    : IF LPAREN expression MENORQUE expression RPAREN DOSPUNTOS
+    | IF LPAREN expression DIFERENTE expression RPAREN DOSPUNTOS
+    | IF LPAREN expression DIGUAL expression RPAREN DOSPUNTOS
+    | IF LPAREN expression MAYORQUE expression RPAREN DOSPUNTOS
+    | IF LPAREN condition RPAREN DOSPUNTOS
+    '''
+    
+def p_else(p):
+    ''' else : ELSE
+    | ELSE TAB '''
+
+def p_expresion_numpy(p):
+    'exprenumpy : NUMPY POINT numpyfunc'
+    p[0] = p[1]+ str(p[2])+p[3]
+
+
+def p_numpyfuncion(p):
+    '''numpyfunc : ARRAY numpyarg
+                 | SUM numpyarg
+                 | RESHAPE numpyarg
+                 | MEAN numpyarg'''
+
+    p[0] = p[1]+p[2]
+
+
+def p_numpyarg(p):
+    '''numpyarg : LPAREN NAME RPAREN'''
+
+    p[0]= "(" + p[2] +")"
+    print(p[0])
+
+def p_expression_uminus(p):
+    'expression : MINUS expression %prec UMINUS'
+    p[0] = -p[2]
+
+def p_expression_group(p):
+    'expression : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+def p_expression_number(p):
+    'expression : NUMBER'
+    p[0] = p[1]
+
+def p_expresion_str(p):
+    'expression : STR'
+    p[0]= str(p[1])
+
+def p_expresion_list(p):
+    'expression : LIST'
+    p[0]=p[1]
+
+
+def p_expression_name(p):
+    'expression : NAME'
+    try:
+        p[0] = names[p[1]]
+        print(names)
+    except LookupError:
+        print("La variable '%s' no esta definida " % p[1])
+
+def p_error(p):
+    if(p!=None): print("Error de sintaxis '%s'" % p)
+
+import ply.yacc as yacc
+parser=yacc.yacc()
+
+def validar (data):
+        try:
+            list_temp=[]
+            lex.input(data)
+            while True:
+                tok = lex.token()
+                if not tok:
+                    break
+                list_temp.append(tok.type)
+            print(list_temp)   
+                
+            result=parser.parse(data)
+            if(result!=None):
+                print(result)
+            
+        except EOFError:
+            print("error lexer")
---
+++ main.py
@@ -0,0 +1,132 @@
+﻿from tkinter import *
+import tkinter.messagebox
+from tkinter.ttk import Frame, Label, Entry
+from lex import validar
+from io import StringIO
+import io
+import sys
+import re
+r = {}
+aux="import numpy as np\n"
+class App(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        problema1 = "Crear un arreglo de numpy basado en una lista"
+        problema2= "Comparar si el primer elemento de una lista es mayor que el segundo\n elemento(imprimir el elemento mayor)"
+        problema3 = "Genere un arreglo de 9 elementos con números pares mayores a 10"
+        problema4 = "Dado el arreglo de numpy del ejercicio anterior utilice una función \nde numpy para hacer posible la multiplicación con su matriz identidad"
+        problema5 = "Dado el arreglo de numpy con las notas de algunos alumnos de matemáticas\n, determine el promedio de las notas"
+        problema6 = "Dados dos arreglos de estudiantes y sus notas, concatene el arreglo e imprima las\nnotas y el nombre del alumno"
+        problema7 = "Dada la matriz de notas y estudiantes obtenga el promedio por estudiante y guarde \nlos promedios en un arreglo"
+        problema8 = "Dado el arreglo anterior determine el mejor promedio e indique si este promedio\nsupera la nota de 9.5"
+        problema9 = "Dado el arreglo de Strings con los nombre de las materias de un curso concatenarlo\ncon otro arreglo de notas para que forme una matriz de 2x10, guarde el resultado en una variable de nombre ClaseA."
+        problema10 ="Dado la matriz del ejercicio anterior y la matriz Clase B que representa las notas de\nel paralelo b. Compare los resultados de las 3 materias principales : Lenguaje,Matemáticas y Ciencias, si el promedio de la clase A supera al de la clase B, guarde\nTrue en un arreglo de numpy, de lo contrario guarde False, finalmente indique en cuántas de las 3 materias el paralelo A es mejor que el B"
+        listProblemas = [problema2,problema3,problema4,problema5,problema6,problema7,problema8, problema9,problema10]
+        global contProblemas
+        contProblemas = 1
+        self.parent.title("Numpy If-else problems")
+        self.pack(fill=BOTH, expand=True)
+        global value
+        value = 0
+        global expr
+        expr = StringVar()
+        global res
+        res = StringVar()
+        randomIterator = iter(listProblemas)
+
+        def cambio(event):
+
+            label.configure(text=next(randomIterator))
+            label.update()
+
+        def validate():
+            count=0
+            if expr.get() == '':
+                str = entry1.get("1.0","end-1c")
+                lista =str.split("\n")
+                lista_code=str.split("\n")
+                for x in lista:
+                    y=re.sub("\t","",x)
+                    lista[count]=y
+                    count=count+1
+    
+                l_exec=[aux]
+                for i in lista_code:
+                    print("enviado:"+i)
+                    validar(i)
+                    l_exec.append(i+'\n')
+                    
+                a=''.join(l_exec)
+                old_stdout = sys.stdout
+                redirected_output = sys.stdout = StringIO()
+                exec(a)
+                sys.stdout = old_stdout
+                result.configure(text=redirected_output.getvalue())
+                result.update()
+                    
+
+
+
+
+            else:
+                print(entry1.get("1.0",END))
+                #result = validate(expr.get())
+                #file = open('res', 'r')
+                #last_line = file.read().splitlines()[-1]
+                #res.set(last_line)
+
+
+
+        frame0 = Frame(self)
+        frame0.pack(fill=X)
+        label = Label(frame0, text=problema1, justify=CENTER)
+        label.pack(side=TOP,padx=50, pady=20)
+        frame1 = Frame(self)
+        frame1.pack(fill=X)
+        btn = Button(frame0,text="Siguiente problema")
+        btn.bind("<Button-1>", cambio)
+        btn.pack(pady=10)
+
+
+
+
+        lbl1 = Label(frame1, text="Your code :", width=18)
+        lbl1.pack(side=LEFT, padx=5, pady=5)
+
+        entry1 = Text(frame1,width=10, height=20)
+        entry1.pack(fill=X, padx=5, expand=True)
+
+        frame3 = Frame(self)
+        frame3.pack(fill=X)
+
+        btnplus = Button(frame3, text="Run", width=8, command=validate)
+        btnplus.pack(side=LEFT, anchor=N, padx=5, pady=5)
+
+        frame4 = Frame(self)
+        frame4.pack(fill=X)
+
+        lbl3 = Label(frame4, text="Result :", width=10)
+        lbl3.pack(side=LEFT, padx=5, pady=5)
+
+        result = Label(frame4,text=r)
+        result.pack(fill=X, padx=5, expand=True)
+
+    def errorMsg(self,msg):
+        if msg == 'error':
+            tkinter.messagebox.showerror('Error!', 'Enter your expression')
+
+
+
+def main():
+    
+    root = Tk()
+    root.geometry("800x600")
+    app = App(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
